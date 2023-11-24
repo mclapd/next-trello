49b5e3813bab9bcdff13d4b54fe51c2b9f64cd5b
one_punch_man <ckim@banlaw.com>
2023-11-25 08:27:14 +1100

    server action

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/form-button.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { useFormStatus } from "react-dom";
+
+export const FormButton = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <div>
+      <Button type="submit" disabled={pending}>
+        Submit
+      </Button>
+    </div>
+  );
+};
