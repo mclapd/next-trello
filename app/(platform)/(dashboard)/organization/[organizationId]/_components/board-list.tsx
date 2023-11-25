f6c2c7a6fd673c5c823a5d5c5259b2056808a216
one_punch_man <ckim@banlaw.com>
2023-11-25 23:32:52 +1100

    Board Server Action

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -10,7 +10,7 @@ export const BoardList = () => {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        <FormPopover sideoffset={10} side="right">
+        <FormPopover sideOffset={10} side="right">
           <div
             role="button"
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
