884a6df479c7dcb08f04e5f3b55127b40ca6e417
one_punch_man <ckim@banlaw.com>
2023-12-14 23:07:06 +1100

    note sidbar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/notes/layout.tsx
@@ -0,0 +1,23 @@
+import Navigation from "./_components/navigation";
+import { Info } from "../_components/info";
+import { Separator } from "@/components/ui/separator";
+import { checkSubscription } from "@/lib/subscription";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
+const NoteLayout = async ({ children }: { children: React.ReactNode }) => {
+  const isPro = await checkSubscription();
+
+  return (
+    <div className="w-full mb-20">
+      <Info isPro={isPro} />
+      <Separator className="my-4" />
+      <div className="h-full flex dark:bg-[#1F1F1F]">
+        <Navigation />
+        <main className="flex-1 h-full overflow-y-auto">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default NoteLayout;
--- app/(platform)/(dashboard)/organization/[organizationId]/notes/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/notes/page.tsx
@@ -5,18 +5,12 @@ import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
 
 const NotesPage = async () => {
-  const isPro = await checkSubscription();
-
   return (
-    <div className="w-full mb-20">
-      <Info isPro={isPro} />
-      <Separator className="my-4" />
-      <div className="h-full flex flex-col items-center justify-center space-y-4">
-        <Button disabled={true}>
-          <PlusCircle className="h-4 w-4 mr-2" />
-          Create a note
-        </Button>
-      </div>
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Button disabled={true}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
     </div>
   );
 };
